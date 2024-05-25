7cb3b1c5bbc802bb4bd0c6a4d7503c2ebf9d34df
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-25 20:50:42 +0530

    community page fix

--- components/Profiles/ProfileList.tsx
+++ components/Profiles/ProfileList.tsx
@@ -7,7 +7,7 @@ import { Separator } from "../ui/separator";
 const ProfileList = ({ profiles }: { profiles: ProfileWithProjects[] }) => {
   return (
     <div className="">
-      <h3 className="text-2xl underline mb-2 ">Verified Members</h3>
+      <h3 className="text-2xl underline mb-2 ">Members</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-3 mb-6">
         {profiles.map((profile) => {
           return (
